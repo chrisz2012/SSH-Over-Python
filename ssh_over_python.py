7fc36f9a644c045c16d21e1c74df6c22232252ec
chrisz2012 <36281493+chrisz2012@users.noreply.github.com>
2022-09-12 18:03:03 -0500

    Create ssh_over_python.py

---
+++ ssh_over_python.py
@@ -0,0 +1,22 @@
+import paramiko
+import sys
+
+firstArg=sys.argv[1]
+secondArg=sys.argv[2]
+
+# Update the next three lines with your
+# server's information
+
+host = firstArg
+username = "YOUR_USER"
+password = "YOUR_PASSWORD"
+command = secondArg
+
+client = paramiko.client.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(host, username=username, password=password)
+_stdin, _stdout,_stderr = client.exec_command(secondArg)
+print(_stdout.read().decode())
+
+del _stdin
+client.close()
