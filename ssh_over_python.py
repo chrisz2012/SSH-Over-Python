9d4d9539e6aeb44254e6e6362e0bd1b3fc7dfbc5
chrisz2012 <36281493+chrisz2012@users.noreply.github.com>
2022-09-12 18:56:26 -0500

    Update ssh_over_python.py

--- ssh_over_python.py
+++ ssh_over_python.py
@@ -1,22 +1,29 @@
 import paramiko
 import sys
 
+# You can put multiple hosts into the firstArg and the script will iterate over them
+# The script splits each host out by just a space in the firstArg
+# i.e. "hostname.eng.com hostname2.eng.com"
 firstArg=sys.argv[1]
 secondArg=sys.argv[2]
 
-# Update the username and password with your
-# credential information
+# Update the next three lines with your
+# server's information
 
 host = firstArg
-username = "YOUR_USER"
+username = "YOUR_USERNAME"
 password = "YOUR_PASSWORD"
 command = secondArg
 
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(host, username=username, password=password)
-_stdin, _stdout,_stderr = client.exec_command(secondArg)
-print(_stdout.read().decode())
 
-del _stdin
+# Added the ability to iterate over multiple hosts
+for i in firstArg.split():
+    print("sshing to {}".format(i))
+    client.connect(i, username=username, password=password)
+    _stdin, _stdout, _stderr = client.exec_command("df")
+    print(_stdout.read().decode())
+    del _stdin
+
 client.close()
